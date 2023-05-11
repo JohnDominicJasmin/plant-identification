ced46116ad3841541222640731d43c0e542e5647
John_Dominic <johndominicjasmin@gmail.com>
2023-05-12 00:55:14 +0800

    Update UI.

--- app/src/main/java/com/example/plantidentification/feature_plant_identification/presentation/guidelines/GuidelinesScreen.kt
+++ app/src/main/java/com/example/plantidentification/feature_plant_identification/presentation/guidelines/GuidelinesScreen.kt
@@ -1,45 +1,33 @@
 package com.example.plantidentification.feature_plant_identification.presentation.guidelines
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.navOptions
-import com.example.plantidentification.R
-import com.example.plantidentification.feature_plant_identification.presentation.choosing_image.MainState
 
 @Composable
 fun GuidelinesScreen(
     navController: NavController,
     onNavigateToPlantInfoCompleted: () -> Unit = {}) {
 
-
-
-
-    GuidelinesContent(onClickStartButton = {
+    GuidelinesContent(onClickContinueButton = {
 
         onNavigateToPlantInfoCompleted()
         navController.navigate("choosing-image", navOptions {
@@ -51,64 +39,66 @@ fun GuidelinesScreen(
 }
 
 @Composable
-fun GuidelinesContent(onClickStartButton: () -> Unit = {}) {
-    val context = LocalContext.current
+fun GuidelinesContent(onClickContinueButton: () -> Unit = {}) {
     Surface(
         modifier = Modifier
             .fillMaxSize(),
-        color = MaterialTheme.colors.background) {
+        color = Color(0xFFF1F5F8)) {
 
         Box(modifier = Modifier.fillMaxSize()) {
 
-            Image(
-                painter = painterResource(id = R.drawable.image_13),
-                contentDescription = "Information Background",
-                modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.FillBounds,
-                alpha = 0.9f,
-            )
 
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(all = 12.dp),
+                    .padding(horizontal = 16.dp, vertical = 12.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.spacedBy(22.dp)) {
 
                 Text(
                     modifier = Modifier.padding(top = 12.dp),
-                    text = "Information",
+                    text = "How to use LEAFGraphy",
                     fontSize = TextUnit(value = 26f, type = TextUnitType.Sp),
                     fontWeight = FontWeight.Bold,
-                    color = Color.White,
+                    color = Color.Black,
                     textAlign = TextAlign.Center)
 
 
-                Text(
-                    modifier = Modifier.padding(all = 15.dp),
-                    text = context.getString(R.string.guidelines),
-                    style = TextStyle(
-                        textDecoration = TextDecoration.Underline,
-                        textAlign = TextAlign.Justify,
-                        fontSize = TextUnit(value = 16f, type = TextUnitType.Sp),
-                        lineHeight = 28.sp,
-                        color = Color.White
-                    ), overflow = TextOverflow.Clip)
+                Text(fontWeight = FontWeight.SemiBold,
+                    fontSize = TextUnit(value = 18f, TextUnitType.Sp),
+                    text = buildAnnotatedString {
+                        withStyle(style = SpanStyle(color = Color.Black)) {
+                            append("Step 1: First, make sure that you have an Internet Connection while using this app.\n\n")
+                        }
+                        withStyle(style = SpanStyle(color = Color.Black)) {
+                            append("Step 2: Allow Permissions for Camera and Storage.\n\n")
+                        }
+                        withStyle(style = SpanStyle(color = Color.Black)) {
+                            append("Step 3: Take a photo or select from gallery of whole Leaves (Note: Crop Leaves and photo with dark lightning can't be recognized)\n\n")
+                        }
+                        withStyle(style = SpanStyle(color = Color.Black)) {
+                            append("Step 4: Click the Button (✔) if you are done taking and uploading photo\n\n")
+                        }
+                        withStyle(style = SpanStyle(color = Color.Black)) {
+                            append("Step 5: If the screen shows (Not Found), you need to retake a photo again.")
+                        }
+                    }
+                )
+
+
+
 
 
                 Spacer(modifier = Modifier.weight(0.6f))
 
-                OutlinedButton(
-                    onClick = onClickStartButton,
-                    modifier = Modifier.padding(all = 4.dp), shape = RoundedCornerShape(30),
-                    colors = ButtonDefaults.outlinedButtonColors(backgroundColor = Color.Transparent),
-                    border = BorderStroke(1.5.dp, Color.White)) {
-                    Text(
-                        text = "Start",
-                        fontSize = TextUnit(value = 14f, type = TextUnitType.Sp),
-                        color = Color.White,
-                        modifier = Modifier.padding(horizontal = 30.dp, vertical = 4.dp))
+                Button(
+                    onClick = onClickContinueButton,
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+                    shape = RoundedCornerShape(12.dp),
+                ) {
+                    Text(text = "Continue", fontSize = TextUnit(value = 14f, TextUnitType.Sp),    modifier = Modifier.padding(horizontal = 30.dp, vertical = 4.dp))
                 }
+
                 Spacer(modifier = Modifier.weight(0.1f))
 
             }
--- app/src/main/java/com/example/plantidentification/feature_plant_identification/presentation/splash_screen/SplashScreen.kt
+++ app/src/main/java/com/example/plantidentification/feature_plant_identification/presentation/splash_screen/SplashScreen.kt
@@ -1,14 +1,23 @@
 package com.example.plantidentification.feature_plant_identification.presentation.splash_screen
 
+import android.content.res.Configuration
 import android.view.animation.OvershootInterpolator
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
@@ -16,7 +25,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.navOptions
 import com.example.plantidentification.R
@@ -26,39 +43,92 @@ import kotlinx.coroutines.delay
 @Composable
 fun SplashScreen(navController: NavController, state: MainState) {
 
-    val navigationScreenChanges = remember(state.startingDestination) {
-        state.startingDestination.isNotEmpty()
-    }
-    val scale = remember { Animatable(initialValue = 0f) }
-    LaunchedEffect(key1 = true, key2 = navigationScreenChanges) {
-        scale.animateTo(0.75f,
-            animationSpec = tween(
-                durationMillis = 700,
-                easing = { OvershootInterpolator(1.2f).getInterpolation(it) }
-            ))
-        delay(1200L)
+    SplashScreenContent(onClickContinue = {
         navController.navigate(route = state.startingDestination, navOptions {
             this.popUpTo("splash-screen") {
                 inclusive = true
             }
         })
-    }
+    })
 
 
+}
 
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colors.background)) {
+
+@Composable
+fun SplashScreenContent(onClickContinue: () -> Unit = {}) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopCenter) {
+
 
         Image(
-            painter = painterResource(id = R.drawable.ic_app_icon),
-            contentDescription = "App Icon",
-            modifier = Modifier
-                .aspectRatio(2f)
-                .scale(scale.value))
+            painter = painterResource(id = R.drawable.batangas_lakelands_review_14_1__8_),
+            contentDescription = "Background Image",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillHeight,
 
+            )
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween) {
+
+
+            Image(
+                modifier = Modifier
+                    .padding(top = 40.dp)
+                    .scale(1.4f),
+                painter = painterResource(id = R.drawable.logoooooooooooooooooooo),
+                contentDescription = "Logo Batangas Lakelands")
+
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(
+                    15.dp,
+                    alignment = Alignment.CenterVertically)
+            ) {
+
+                Text(
+                    text = "Welcome To \nLEAFGraphy",
+                    style = MaterialTheme.typography.h4,
+                    color = Color.White,
+                    modifier = Modifier
+
+                        .align(alignment = Alignment.CenterHorizontally),
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    fontSize = TextUnit(value = 27f, TextUnitType.Sp)
+                )
+
+                Text(
+                    text = "Your personal assistant for \nrecognizing the tree species",
+                    color = Color.White,
+                    textAlign = TextAlign.Justify,
+                    modifier = Modifier
+                        .align(alignment = Alignment.CenterHorizontally),
+                    fontSize = TextUnit(value = 16f, TextUnitType.Sp)
+                )
+
+
+                IconButton(
+                    onClick = onClickContinue, modifier = Modifier
+                        .scale(1.1f)
+                        .padding(top = 50.dp)) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.arrrowww),
+                        contentDescription = "Continue Arrow",
+                        tint = Color.White)
+                }
+
+
+            }
+        }
     }
+}
 
+@Preview
+@Composable
+fun PreviewSplashScreen() {
+    SplashScreenContent()
 }
